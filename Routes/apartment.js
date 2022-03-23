c4eff97ba40604b065ff754e72fa62bd6c503b3d
tawfik shalsh <t.shalash1@gmail.com>
2022-03-23 09:41:15 +0200

    apartment.js and booking add end points

--- Routes/apartment.js
+++ Routes/apartment.js
@@ -4,9 +4,45 @@ const pool = require("../db");
 
 
 
+router.post("/", async (req, res) => {
+    try {
+        console.log(req.body);
+        await pool.query(
+            "Insert Into apartment(apartment_id,address,rooms,description,price,free,landlord_id) values(?,?,?,?,?,?,?)",
+            [req.body.apartment_id, req.body.address, req.body.rooms, req.body.description, req.body.price, req.body.free, req.body.landlord_id]);
+
+        res.json("apartment is added");
+    }
+    catch (err) {
+        console.log(err.message);
+        res.json("error while inserting the data");
+    }
+});
+
+router.get("/", async (req, res) => {
+    try {
+        const apartment = await pool.query("select apartment_id,address,rooms,description,price,free,landlord_id from apartment");
+        console.log(apartment);
+        res.json(apartment[0]);
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(400).send({ message: 'error!' });
+    }
+});
+
+router.delete("/:id", async (req, res) => {
+    try {
+
+        await pool.query("DELETE FROM apartment WHERE apartment_id = ?", [req.params.id]);
+
+        res.json("element  is deleted");
+    } catch (err) {
+        console.error(err.message);
+        res.status(400).send({ message: 'error!' });
+    }
+});
 
 
 
-
-
-module.exports=router;
+module.exports = router;
--- Routes/booking.js
+++ Routes/booking.js
@@ -5,8 +5,53 @@ const pool = require("../db");
 
 
 
+router.post("/", async (req, res) => {
+    try {
+        console.log(req.body);
+        //need to check if its valid
+
+        await pool.query(
+            "Insert Into booking(booking_id,in_date,out_date,apartment_id,tenant_id) values(?,?,?,?,?)",
+            [req.body.booking_id, req.body.in_date, req.body.out_date, req.body.apartment_id, req.body.tenant_id]);
+
+        //make the apartment not free
+
+        res.json("new booking is added");
+    }
+    catch (err) {
+        console.log(err.message);
+        res.json("error while inserting the data");
+    }
+});
+
+router.get("/", async (req, res) => {
+    try {
+        const booking = await pool.query("select booking_id,in_date,out_date,apartment_id,tenant_id from booking");
+        console.log(booking);
+
+        res.json(booking[0]);
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(400).send({ message: 'error!' });
+    }
+});
+
+router.delete("/:id", async (req, res) => {
+    try {
+
+        await pool.query("DELETE FROM booking WHERE booking_id = ?", [req.params.id]);
+
+        //update the apartment table
+
+        res.json("element  is deleted");
+    } catch (err) {
+        console.error(err.message);
+        res.status(400).send({ message: 'error!' });
+    }
+});
 
 
 
 
-module.exports=router;
+module.exports = router;
